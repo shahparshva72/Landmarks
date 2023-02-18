9ba705d5791d7ffc0ce5415db3ef81fea7f83222
Parshva Shah <shahparshva72@gmail.com>
2023-02-18 02:10:46 -0600

    Animating Views and Transitions ✅

--- Landmarks/Views/Landmarks/LandmarkList.swift
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -8,32 +8,31 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
-    
+
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
-    
+
     var body: some View {
         NavigationView {
             List {
                 Toggle(isOn: $showFavoritesOnly) {
-                    Text("Show Favorites Only")
+                    Text("Favorites only")
                 }
-                
+
                 ForEach(filteredLandmarks) { landmark in
-                    NavigationLink{
+                    NavigationLink {
                         LandmarkDetail(landmark: landmark)
                     } label: {
                         LandmarkRow(landmark: landmark)
                     }
                 }
-                .navigationTitle("Landmarks")
             }
+            .navigationTitle("Landmarks")
         }
     }
 }
